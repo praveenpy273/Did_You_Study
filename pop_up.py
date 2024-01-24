c0aba41269812ba080c6b06a8a81bf9951fb1e1f
Praveen Paliwal <praveenpaliwal273@gmail.com>
2024-01-24 20:39:41 +0000

    Calculate days left

--- pop_up.py
+++ pop_up.py
@@ -1,9 +1,16 @@
-from datetime import date
+from datetime import date,datetime
 from dateutil import parser
 question = print('What is the target date: ')
 answer = input()
 try:
     target_date = parser.parse(answer).date()
-    print('Parsed_Date',target_date)
+    print('Target_Date',target_date)
 except ValueError:
     print('invalid date_format')
+
+todays_date = datetime.now().strftime('%d-%m-%Y')
+current_date = parser.parse(todays_date).date()
+print(current_date)
+
+days_left = target_date - current_date
+print(days_left.days)
