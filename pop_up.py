9131dbbaa806dc12fb0804bc68d36334d1e9d971
Praveen Paliwal <praveenpaliwal273@gmail.com>
2024-01-26 21:05:33 +0000

    modified code to get data for a single day

--- pop_up.py
+++ pop_up.py
@@ -1,7 +1,9 @@
-from datetime import date,datetime
+from datetime import date, datetime, timedelta
 from dateutil import parser
 import tkinter as tk
 from tkinter import messagebox, simpledialog
+import pandas as pd
+import math
 question = print('What is the target date: ')
 answer = input()
 try:
@@ -23,33 +25,48 @@ window = tk.Tk()
 window.attributes('-topmost', True)
 window.withdraw() #Hide main window
 
+
 # Prompting users for their input
 
 daily_ques = messagebox.askquestion('Did you study today? ', icon = 'info')
 print("User's response", daily_ques)
 if daily_ques == 'yes':
     hours_ques = simpledialog.askinteger('Good!', 'How many hours did you study? ', parent=window)
+
+
     if hours_ques < 3:
         total_hours += hours_ques
     else:
         total_hours -= hours_ques
 else:
     total_hours += 3
+    hours_ques = 0
+
         
 print('Revised total hours: ', total_hours)
-still_days_left_to_achieve_target = total_hours / 3
+still_days_left_to_achieve_target = math.ceil(total_hours / 3)
 
 print('Revised days left: ', still_days_left_to_achieve_target)
 
 
 if still_days_left_to_achieve_target > days_left_to_achieve_target:
-    trend =  still_days_left_to_achieve_target - days_left_to_achieve_target
-    print(trend)
-    if trend > 10:
+    negative_trend =  days_left_to_achieve_target - still_days_left_to_achieve_target
+    print('trend is: ', negative_trend)
+    
+    if negative_trend < -10:
         messagebox.showwarning('Warning','What are you doing, work hard or forget!')
     else:
         messagebox.showwarning(Warning,'Need to pick up!')
 else:
     messagebox.showinfo('Info','Good going!')
+    possitive_trend =  still_days_left_to_achieve_target > days_left_to_achieve_target
+    
+data = {'Date': current_date, 'Hours_studied' : hours_ques, 'Initial_target_days': days_left_to_achieve_target,
+         'Revised_Days_Left' : still_days_left_to_achieve_target}
+
+
+df = pd.DataFrame([data],index = [0])
+print(df)
+
 window.deiconify() # Show the window briefly
 window.destroy()
