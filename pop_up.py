4648a9575f2d9a86f6014dcc681605beef650fc5
Praveen Paliwal <praveenpaliwal273@gmail.com>
2024-02-08 20:44:22 +0000

    updated code to add new rows, calculate actual days,likely target value and study trend, alos renamed csv file

--- pop_up.py
+++ pop_up.py
@@ -7,6 +7,34 @@ import math
 import os
 import matplotlib.pyplot as plt
 
+def likely_target_date(trgt,org_days_left,act_days_left):
+    difference_after_yesterday_hours = act_days_left - org_days_left
+    likely_target_acheive_date = trgt + timedelta(difference_after_yesterday_hours)
+    return likely_target_acheive_date
+
+
+def find_actual_hours(hrs_std,org_hrs): 
+    if hrs_std == 0:
+        act_hrs = org_hrs + 3
+    elif 0 < hours_ques < 3:
+        act_hrs = org_hrs + (3-hrs_std)
+    elif hours_ques >= 3:
+        act_hrs = org_hrs - hrs_std
+    
+    return act_hrs
+
+
+def find_trend(org_days_left,actual_days_left, trend = 0):
+    diff = actual_days_left - org_days_left
+    if diff == 0:
+        trend = 0
+    elif diff > 0:
+        trend -= diff
+    else:
+        trend += diff
+
+    return trend
+
 question = print('What is the target date: ')
 answer = input()
 try:
@@ -20,68 +48,49 @@ todays_date = datetime.now().date()
 yesterday = todays_date-timedelta(days=1)
 print('Yesterday was: ', yesterday)
 
-days_left = target_date - todays_date
-days_left_to_achieve_target = days_left.days
+days_left = target_date - yesterday
+org_days_left = days_left.days
 print('Days left to achieve target: ', days_left.days)
-total_hours = days_left_to_achieve_target * 3
-print('Total hours to acheive target: ', total_hours)
+original_hours_left = org_days_left * 3 #calculates the hours left to acheive target
+print('Total hours to acheive target: ', original_hours_left)
 window = tk.Tk()
 window.attributes('-topmost', True)
 window.withdraw() #Hide main window
-
 
 # Prompting users for their input
 
 daily_ques = messagebox.askquestion('Did you study yesterday? ', icon = 'info')
 print("User's response", daily_ques)
 if daily_ques == 'yes':
-    hours_ques = simpledialog.askinteger('Good!', 'How many hours did you study? ', parent=window)
-
-
-    if hours_ques == 0:
-        total_hours += 3
-    elif 0 < hours_ques < 3:
-        total_hours += hours_ques
-    else:
-        total_hours -= hours_ques
+    hours_ques = simpledialog.askinteger('Good!', 'How many hours did you study? ', parent=window) # hours studied yesterday
+    actual_hours_left = find_actual_hours(hours_ques,original_hours_left)
 
 else:
-    total_hours += 3
     hours_ques = 0
-
+    actual_hours_left = find_actual_hours(hours_ques,original_hours_left)
         
-print('Revised total hours: ', total_hours)
-still_days_left_to_achieve_target = math.ceil(total_hours / 3)
+print('Revised total hours: ', actual_hours_left) # hours left after adjusting yesterday's 
 
-print('Revised days left: ', still_days_left_to_achieve_target)
+actual_days_left = math.ceil(actual_hours_left / 3)
 
+# difference_after_yesterday_hours = actual_days_left - org_days_left
 
-if still_days_left_to_achieve_target > days_left_to_achieve_target:
-    negative_trend =  days_left_to_achieve_target - still_days_left_to_achieve_target
-    positive_trend = 0
-    print('trend is: ', negative_trend)
-    
-    if negative_trend < -10:
-        messagebox.showwarning('Warning','What are you doing, work hard or forget!')
-    else:
-        messagebox.showwarning(Warning,'Need to pick up!')
-else:
-    messagebox.showinfo('Info','Good going!')
-    positive_trend =  days_left_to_achieve_target - still_days_left_to_achieve_target
-    negative_trend = 0
-    
-data = {'Date': [yesterday], 'Hours_studied' : [hours_ques], 'Initial_target_days': [days_left_to_achieve_target],
-         'Revised_Days_Left' : [still_days_left_to_achieve_target], 
-         'Negative_Trend': [negative_trend] if negative_trend else [0],
-         'Positive_Trend': [positive_trend] if positive_trend else [0]}
+likely_target_acheive_date = likely_target_date(target_date,org_days_left,actual_days_left)
+print(likely_target_acheive_date)
 
+data = {'Date': [yesterday], 'Hours_studied' : [hours_ques], 'Original_Target_Date' : [target_date],
+        'Likely_Target_Date': [likely_target_acheive_date],
+        'Initial_target_days': [org_days_left],
+        'Revised_Days_Left' : [actual_days_left],
+        'Hours_left_for_target': [original_hours_left],
+        'Actual_Hours_Left' : find_actual_hours(hours_ques,original_hours_left),
+        'Trend': [find_trend(org_days_left,actual_days_left)]}
 
 df = pd.DataFrame(data)
 print(df)
 
-
 # Saving datafrafe to csv file
-csv_file = 'study_tracker.csv'
+csv_file = 'test_study_tracker.csv'
 
 # Check if the file exists if not create the file
 if not os.path.isfile(csv_file):
@@ -93,26 +102,56 @@ else:
     print('file found!')
     df_loaded = pd.read_csv(csv_file)
     print('Before concat: ', df_loaded)
-    if str(yesterday) in df_loaded['Date'].astype(str).values:
+    if str(yesterday) in df_loaded['Date'].astype(str).values: #if the entry for a date is already there and want to adjust the hours
         print('file found!, but date already exists')
         df_loaded.loc[df_loaded['Date'].astype(str) == str(yesterday), ['Hours_studied']] = hours_ques
-
+        df_loaded.loc[df_loaded['Date'].astype(str) == str(yesterday), ['Actual_Hours_Left']] = find_actual_hours(hours_ques,original_hours_left)
+        df_loaded.loc[df_loaded['Date'].astype(str) == str(yesterday), ['Revised_Days_Left']] = math.ceil(actual_hours_left/3)
+        df_loaded.loc[df_loaded['Date'].astype(str) == str(yesterday), ['Likely_Target_Date']] = likely_target_date(target_date,org_days_left,actual_days_left)
+        df_loaded.loc[df_loaded['Date'].astype(str) == str(yesterday), ['Trend']] = \
+            find_trend(org_days_left,actual_days_left)
     else:
-        new_data = {'Date': [yesterday], 'Hours_studied' : [hours_ques], 'Initial_target_days': [days_left_to_achieve_target],
-         'Revised_Days_Left' : [still_days_left_to_achieve_target], 
-         'Negative_Trend': [negative_trend] if negative_trend else [0],
-         'Positive_Trend': [positive_trend] if positive_trend else [0]}
+        l = len(df_loaded) #length of existing dataframe
+        print('hours_studied:', hours_ques)
+        print('rows in df: ', l)
+    
+        actual_hours_left = find_actual_hours(hours_ques,df_loaded['Actual_Hours_Left'].iloc[l-1])
+        print('else block', actual_hours_left)
+
+        actual_days_left = math.ceil(actual_hours_left/3)
+        likely_target_acheive_date = likely_target_date(target_date,org_days_left,actual_days_left)
+
+        # creating data for new rows
+        new_data = {'Date': [yesterday], 'Hours_studied' : [hours_ques], 'Original_Target_Date' : [target_date],
+        'Likely_Target_Date': [likely_target_acheive_date],
+        'Initial_target_days': [org_days_left],
+        'Revised_Days_Left' : [actual_days_left],
+        'Hours_left_for_target': [original_hours_left],
+        'Actual_Hours_Left' : [actual_hours_left],
+        'Trend' :find_trend(org_days_left,actual_days_left)}
+
         new_df = pd.DataFrame(new_data)
         df_loaded['Date'] = df_loaded['Date'].astype(str)
         new_df['Date'] = new_df['Date'].astype(str)
-        common_columns = df_loaded.columns.intersection(new_df.columns)
-
+        common_columns = df_loaded.columns.intersection(new_df.columns)#finding common columns
+        
+        # concattenating 2 dataframes
         df_loaded = pd.concat([df_loaded[common_columns],new_df[common_columns]],ignore_index=True)
         print('After concat: ', df_loaded)
     
     df_loaded.to_csv(csv_file, index=False, header=True)
     
-            
+func_trend = find_trend(org_days_left,actual_days_left)
+
+if 0 > func_trend > -10:
+    messagebox.showwarning(Warning,'Need to pick up!')
+
+elif func_trend <= -10:
+    messagebox.showwarning('Warning','What are you doing, work hard or forget!')
+
+else:
+    messagebox.showinfo('Info','Good going!')
+
 
 # Displaying the progress in plot
 plt.figure(figsize=(12,8))
@@ -127,18 +166,16 @@ plt.ylabel('Hours Studied')
 plt.legend()
 
 #plotting target days movement
-
 plt.subplot(3,1,2)
 plt.plot(df_loaded['Date'], df_loaded['Revised_Days_Left'], marker='o', label='Revised_Days_Left')
 plt.title('Target Days')
 plt.xlabel('Date')
 plt.ylabel('Revised_Days_Left')
 plt.legend()
-#plotting possitive and negative trend
 
+#plotting possitive and negative trend
 plt.subplot(3,1,3)
-plt.plot(df_loaded['Date'], df_loaded['Negative_Trend'], marker='o', label='Negative_Trend')
-plt.plot(df_loaded['Date'], df_loaded['Positive_Trend'], marker='o', label='Positive_Trend')
+plt.plot(df_loaded['Date'], df_loaded['Trend'], marker='o',label='Trend')
 plt.title('Study_tracker trend')
 plt.xlabel('Date')
 plt.ylabel('Values')
